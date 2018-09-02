807b162e7606e58a5d50d29b3602fe6c22d43ec8
cderian <c.derian_11@ciencias.unam.mx>
2018-09-02 15:52:53 -0500

    Construyendo Transport

--- P2/Transport.java
+++ P2/Transport.java
@@ -1,30 +1,65 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Transport{
 
-	private static Transport transport;
+    public final static int CAPACIDAD = 500;
+
+    private static Transport transport;
+    private HashMap<String, LinkedList<Message>> map;
+    private int size;
+
+    /**
+     * Constructor de Transport
+     */
+    private Transport(){
+        map = new HashMap<String, LinkedList<Message>>();
+        size = 0;
+    }
 
     public static Transport getInstance(){
-
-    	if(transport == null){
-    		transport = new Transport();
-    	}
-
-    	return transport;
+        if(transport == null){
+            transport = new Transport();
+        }
+        return transport;
     }
 
-    public boolean put(Message m, String destination){
-    	sleep(200);
-    	return false;
+    public synchronized boolean put(Message m){
+        boolean status = false;
+
+        if(!map.containsKey(m.getDestino())){
+            map.put(m.getDestination(), new LinkedList<Message>());
+        }
+        
+        if(size < CAPACIDAD){
+            map.get(m.getDestino()).add(m);
+            size++;
+            status = true;
+            sleep(10);
+        }
+        
+        return status;
     }
 
-    public Message pop(String n){
-    	sleep(300);
-    	return null;
+    public synchronized Message pop(String key){
+        Message m = null;
+
+        if(map.containsKey(key)){
+            LinkedList<Message> list = map.get(key);
+        
+            if(!list.isEmpty()){
+                m = list.pop();
+                size--;
+                sleep(30);
+            }
+        }
+        return m;
     }
 
     private void sleep(int ms){
-    	try{
-    		Thread.sleep(ms);
-    	}catch(Exception ex){
-    	}
+        try{
+            Thread.sleep(ms);
+        }catch(Exception ex){
+        }
     }
 }
